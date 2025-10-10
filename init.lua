501f6f6e01830bb515462c14a36bc9602ebad138
= <=>
2025-10-09 23:20:19 -0400

    initial commmit

---
+++ init.lua
@@ -0,0 +1,36 @@
+print("init.lua start")
+
+require("config.lazy")
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.wrap = false
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.keymap.set("n", "<leader>of", ":Ex <CR>", { noremap = true })
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", {noremap = true })
+
+-- LSP format the buffer/file
+vim.keymap.set("n", "<C-Space>", function() vim.lsp.buf.format() end)
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",
+  },
+})
+
+
+vim.cmd('highlight MatchParen cterm=bold ctermfg=white guifg=white')
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      -- hrsh7th/nvim-cmp is required for cmp-nvim-lsp to function
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend(
+        "force",
+        capabilities,
+        require("cmp_nvim_lsp").default_capabilities()
+      )
+
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+        },
+        settings = {},
+      })
+
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+        filetypes = {
+          "python",
+        },
+        settings = {},
+      })
+
+
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("pyright")
+
+      -- Configure nvim-cmp with the nvim_lsp source
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+        },
+      })
+
+      -- vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end)
+
+      -- The following function is for auto-format on save.
+      -- vim.api.nvim_create_autocmd('LspAttach', {
+      --   callback = function(args)
+      --     local client = vim.lsp.get_client_by_id(args.data.client_id)
+      --     if not client then return end
+      --
+      --     if client.supports_method('textDocument/formatting') then
+      --       vim.api.nvim_create_autocmd('BufWritePre', {
+      --         buffer = args.buf,
+      --         callback = function() 
+      --           vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+      --         end,
+      --       })
+      --     end
+      --   end,
+      -- })
+
+    end,
+  }
+}
