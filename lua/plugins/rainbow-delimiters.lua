533328d5824b99c7380482d5a3067333213b6c68
Yaakov Eidelman <yeidelman@eidelmans.com>
2025-10-23 21:07:35 -0400

    added rainbow-delimiters for brackets, and parens

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("rainbow-delimiters.setup").setup({
+        -- Your customization here
+      })
+    end,
+  },
+}
