a71fb3881860b2b9ac12a1c951eb3ae36afb604e
Yaakov Eidelman <yeidelman@eidelmans.com>
2025-10-25 23:18:27 -0400

    added colors to the indentation lines

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,28 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+		-- enabled = false,
+		config = function()
+			require("ibl").setup({
+				indent = {
+					-- This links the rainbow highlight groups to the indent lines.
+					highlight = {
+						"RainbowDelimiterRed",
+						"RainbowDelimiterYellow",
+						"RainbowDelimiterBlue",
+						"RainbowDelimiterOrange",
+						"RainbowDelimiterGreen",
+						"RainbowDelimiterViolet",
+						"RainbowDelimiterCyan",
+					},
+				},
+				-- Enable this to use Treesitter, required for nvim-ts-rainbow integration.
+				scope = {
+					enabled = true,
+				},
+			})
+		end,
+	},
 }
