cceb63eb85c9d72d08e770f444fc99488537ad18
Yaakov Eidelman <yeidelman@eidelmans.com>
2025-10-28 19:39:48 -0400

    added persistant-breakpoints

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,24 +1,25 @@
 return {
-  "stevearc/conform.nvim",
-  event = "BufWritePre",
-  config = function()
-    require("conform").setup({
-      format_on_save = {
-        timeout_ms = 2000,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black", "ruff" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
-        javascriptreact = { "prettier" },
-        sh = { "shfmt" },
-        c = { "clang_format" },
-        cpp = { "clang_format" },
-        cs = { "csharpier" },
-      },
-    })
-  end,
+	"stevearc/conform.nvim",
+	event = "BufWritePre",
+	config = function()
+		require("conform").setup({
+			-- format_on_save = {
+			--   timeout_ms = 2000,
+			--   lsp_fallback = true,
+			-- },
+			format_on_save = false,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black", "ruff" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				javascriptreact = { "prettier" },
+				sh = { "shfmt" },
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+				cs = { "csharpier" },
+			},
+		})
+	end,
 }
