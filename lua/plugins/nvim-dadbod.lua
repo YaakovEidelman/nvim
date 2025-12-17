709e8aa5dfc041655c0aa74340e37017513662b5
Yaakov Eidelman <yeidelman@eidelmans.com>
2025-12-17 17:52:02 -0500

    added sql plugins

---
+++ lua/plugins/nvim-dadbod.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "tpope/vim-dadbod",
+        lazy = true,
+        config = function()
+            vim.keymap.set("n", "<leader>eq", "<cmd>DB<CR>", { desc = "Toggle DBUI" })
+        end
+    },
+    {
+        'kristijanhusak/vim-dadbod-completion',
+        ft = { 'sql', 'mysql', 'plsql' },
+        lazy = true
+    },
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+             'tpope/vim-dadbod',
+             "kristijanhusak/vim-dadbod-completion",
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+            vim.keymap.set("n", "<leader>sql", "<cmd>DBUIToggle<CR>", { desc = "Toggle DBUI" })
+        end,
+    }
+}
