6dc9f232c23f04133f1408c7f44b566cd313835c
YaakovEidelman <=>
2026-03-31 18:53:22 -0400

    commented out neotest

--- lua/plugins/nvim-neotest.lua
+++ lua/plugins/nvim-neotest.lua
@@ -1,28 +1,28 @@
 return {
-    { "nvim-neotest/nvim-nio" },
-    {
-        "nvim-neotest/neotest",
-        requires = {
-            "Issafalcon/neotest-dotnet",
-        },
-        dependencies = {
-            "nvim-neotest/nvim-nio",
-            "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        config = function()
-            require("neotest").setup({
-                adapters = {
-                    require("neotest-dotnet")
-                }
-            })
-        end,
-    },
-    {
-        "Issafalcon/neotest-dotnet",
-        dependencies = {
-            "nvim-neotest/neotest",
-        }
-    }
+    -- { "nvim-neotest/nvim-nio" },
+    -- {
+    --     "nvim-neotest/neotest",
+    --     requires = {
+    --         "Issafalcon/neotest-dotnet",
+    --     },
+    --     dependencies = {
+    --         "nvim-neotest/nvim-nio",
+    --         "nvim-lua/plenary.nvim",
+    --         "antoinemadec/FixCursorHold.nvim",
+    --         "nvim-treesitter/nvim-treesitter",
+    --     },
+    --     config = function()
+    --         require("neotest").setup({
+    --             adapters = {
+    --                 require("neotest-dotnet")
+    --             }
+    --         })
+    --     end,
+    -- },
+    -- {
+    --     "Issafalcon/neotest-dotnet",
+    --     dependencies = {
+    --         "nvim-neotest/neotest",
+    --     }
+    -- }
 }
