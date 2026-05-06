2c6fe036362a6b186e996caceb746cd095703661
YaakovEidelman <148411067+YaakovEidelman@users.noreply.github.com>
2026-05-06 16:45:16 -0400

    comment out vim-fugitive for now

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"tpope/vim-fugitive",
-		event = "VeryLazy",
-		keys = {
-			{ "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
-			{ "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
-			{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
-			{ "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
-			{ "<leader>gP", "<cmd>Git pull<cr>", desc = "Git pull" },
-			{ "<leader>gf", "<cmd>Git fetch<cr>", desc = "Git fetch" },
-		},
-	},
+	-- {
+	-- 	"tpope/vim-fugitive",
+	-- 	event = "VeryLazy",
+	-- 	keys = {
+	-- 		{ "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
+	-- 		{ "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
+	-- 		{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
+	-- 		{ "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
+	-- 		{ "<leader>gP", "<cmd>Git pull<cr>", desc = "Git pull" },
+	-- 		{ "<leader>gf", "<cmd>Git fetch<cr>", desc = "Git fetch" },
+	-- 	},
+	-- },
 }
